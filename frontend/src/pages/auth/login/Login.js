0272c7150173621bf456cfcf1a811581c6170498
sachin <sachinnarofficials@gmail.com>
2024-09-03 14:47:24 +0530

    create a verify jwt token method in node js

--- frontend/src/pages/auth/login/Login.js
+++ frontend/src/pages/auth/login/Login.js
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("formData", formData);
+    try {
+      const response = await fetch("http://localhost:5000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData), // Ensure the body is properly formatted
+      });
+      const data = await response.json();
+      const token = data.token;
+      localStorage.setItem("authToken", token);
+      navigate("/dashboard");
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setFormData({ email: "", password: "" });
+    }
+  };
+
+  return (
+    <div className="center-form">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name={"email"}
+            placeholder="name@example.com"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicName" value={formData.password}>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+        <Button variant="dark" type="submit" className="w-100">
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/pages/header/Header.js
+++ frontend/src/pages/header/Header.js
@@ -2,22 +2,44 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("authToken");
+  console.log("token", token);
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    navigate("/login");
+  };
   return (
     <div>
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg={token ? "primary" : "dark"} data-bs-theme="dark">
         <Container>
-          <Navbar.Brand>Not LoggedIn</Navbar.Brand>
-          <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/login" className="nav-link">
-              Login
-            </Nav.Link>
-            <Nav.Link as={Link} to="/register" className="nav-link">
-              Signup
-            </Nav.Link>
-          </Nav>
+          <Navbar.Brand>{token ? "LoggedIn" : "Not LoggedIn"}</Navbar.Brand>
+          {token ? (
+            <>
+              <Nav className="ml-auto">
+                <Nav.Link as={Link} to="/dashboard" className="nav-link">
+                  Dashboard
+                </Nav.Link>
+                <Nav.Link as={Link} className="nav-link" onClick={handleLogout}>
+                  Logout
+                </Nav.Link>
+              </Nav>
+            </>
+          ) : (
+            <>
+              <Nav className="ml-auto">
+                <Nav.Link as={Link} to="/login" className="nav-link">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to="/register" className="nav-link">
+                  Signup
+                </Nav.Link>
+              </Nav>
+            </>
+          )}
         </Container>
       </Navbar>
     </div>
